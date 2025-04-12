3a90dd6c07570484d12cf05d5c72d383e7c6e970
KotyshevaYulya <yulyaxd07@gmail.com>
2025-04-12 20:36:23 +0200

    add markup

---
+++ script.js
@@ -0,0 +1,22 @@
+const accordionButtons = document.querySelectorAll('.accordion-button');
+
+accordionButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const isExpanded = button.getAttribute('aria-expanded') === 'true';
+        const content = button.nextElementSibling;
+
+        // Закриваємо всі акордеони
+        accordionButtons.forEach(btn => {
+            btn.setAttribute('aria-expanded', false);
+            btn.nextElementSibling.classList.remove('open');
+            btn.nextElementSibling.style.maxHeight = null;
+        });
+
+        // Якщо поточний був закритий — відкриваємо
+        if (!isExpanded) {
+            button.setAttribute('aria-expanded', true);
+            content.classList.add('open');
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+});
